0dab63ae147109087eba8129f16d0522774367df
bhavjot <bhavjot.k@prospa.com>
2016-05-23 00:12:24 +1000

    Added Stored Proc and trigger

---
+++ ProcessDatabase/DatabaseTrigger.sql
@@ -0,0 +1,37 @@
+﻿CREATE TRIGGER [DatabaseTrigger_DELETE_PROCESS]
+ON [Process]
+FOR DELETE
+AS
+BEGIN
+DECLARE @processId int
+SELECT @processId = Id from DELETED
+	DECLARE @Delete TABLE
+(
+  id int
+)
+
+;WITH IdsToDelete (id)
+AS (
+  SELECT Id
+  FROM ProcessStep
+  WHERE ProcessId = @processId
+  UNION ALL
+  SELECT s.Id
+  FROM ProcessStep s 
+  INNER JOIN IdsToDelete I ON I.id = s.ParentProcessStepId AND s.ProcessId = @processId
+)
+
+
+
+INSERT INTO @Delete (id)
+  SELECT id
+  FROM IdsToDelete
+
+
+DELETE FROM ProcessViewingHistory WHERE ProcessId = @processId
+Update ProcessStep SET LinkedProcessId = NULL Where LinkedProcessId = @processId
+Update ProcessStep SET ParentProcessStepId = NULL WHERE ParentProcessStepId IN (SELECT Id FROM @Delete) AND ProcessId != @processId
+DELETE FROM ProcessStep WHERE Id IN (SELECT Id FROM @Delete) 
+Update Process SET ParentId = NULL  Where ParentId = @processId
+
+END
---
+++ ProcessDatabase/ProcessDELETE.sql
@@ -0,0 +1,35 @@
+﻿CREATE PROCEDURE DELETE_PROCESS
+  @processId int
+
+AS
+DECLARE @Delete TABLE
+(
+  id int
+)
+
+;WITH IdsToDelete (id)
+AS (
+  SELECT Id
+  FROM ProcessStep
+  WHERE ProcessId = @processId
+  UNION ALL
+  SELECT s.Id
+  FROM ProcessStep s 
+  INNER JOIN IdsToDelete I ON I.id = s.ParentProcessStepId AND s.ProcessId = @processId
+)
+
+
+
+INSERT INTO @Delete (id)
+  SELECT id
+  FROM IdsToDelete
+
+
+DELETE FROM ProcessViewingHistory WHERE ProcessId = @processId
+Update ProcessStep SET LinkedProcessId = NULL Where LinkedProcessId = @processId
+Update ProcessStep SET ParentProcessStepId = NULL WHERE ParentProcessStepId IN (SELECT Id FROM @Delete) AND ProcessId != @processId
+DELETE FROM ProcessStep WHERE Id IN (SELECT Id FROM @Delete) 
+Update Process SET ParentId = NULL  Where ParentId = @processId
+DELETE FROM Process WHERE Id = @processId 
+
+RETURN;
